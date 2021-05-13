b1f2920067a9edb4939cb15e0053280d20181335
manishn12 <manishn12@users.noreply.github.com>
2021-05-14 00:13:58 +0530

    reusable-card

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import Footer from './Footer'
+import {dummy} from '../dummy';
+import Card from './Card';
+
 
 function About() {
+
     return (
     <div>
+   
         <div style={{marginTop:"70px",minHeight:"303px"}}>
-           <h1> This is About Section.</h1>
+           <h1 className="text-center"> This is About Section.</h1>
+
+    <div>
+
+{
+    dummy.map((item,key)=> (
+<Card key={key} heading={item.heading}  description={item.description} body ={item.body} />
+
+))
+
+}
+ 
+   
+
+    </div>
         </div>
         <Footer />
         </div>
